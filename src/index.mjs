e0cace0139c77cfd9247a3d4a60f0e20b8753854
aldorado <anuar.suarez@23degrees.io>
2019-11-21 14:19:24 +0100

    nodeenv correctly

--- src/index.mjs
+++ src/index.mjs
@@ -4,11 +4,11 @@ class Logger {
   }
 
   static info(...args) {
-    console.log(process.env.NODE_ENV === 'development' ? '\x1b[44mINFO\x1b[0m' : new Date(), ' INFO ', ...args);
+    console.log(process.env.NODE_ENV === 'development' ? '\x1b[44mINFO\x1b[0m' : `${new Date()} INFO`, ...args);
   }
 
   static error(...args) {
-    console.error(process.env.NODE_ENV === 'development' ? '\x1b[41mERROR\x1b[0m' : new Date(), ' ERROR ', ...args);
+    console.error(process.env.NODE_ENV === 'development' ? '\x1b[41mERROR\x1b[0m' : `${new Date()} ERROR`, ...args);
   }
 }
 
