68af9d62d2ae44777df2e7365844ef40c7adb056
aldorado <anuar.suarez@23degrees.io>
2019-11-21 10:50:55 +0100

    liquid logging

---
+++ src/index.mjs
@@ -0,0 +1,17 @@
+class Logger {
+  static debug(...args) {
+    if (process.env.NODE_ENV !== 'development') console.log('\x1b[42mDEBUG\x1b[0m', ...args);
+  }
+
+  static info(...args) {
+    console.log(process.env.NODE_ENV !== 'development' ? '\x1b[44mINFO\x1b[0m' : new Date(), ' INFO ', ...args);
+  }
+
+  static error(...args) {
+    console.error(process.env.NODE_ENV !== 'development' ? '\x1b[41mERROR\x1b[0m' : new Date(), ' ERROR ', ...args);
+  }
+}
+
+// const logger = new Logger();
+
+export default Logger;
